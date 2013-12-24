3492211f02b71bbc615c0c8aa0a591dfd5dbab09
Audrius Jurgelionis <ajurge@gmail.com>
2013-12-24 18:27:40 +0200

    Modify the description

--- cpuload.sh
+++ cpuload.sh
@@ -1,15 +1,17 @@
  #!/bin/bash
 ###############################################################################
-###
-###  This script generates a desired CPU load and forces it per each core.
-### It requires the following programs to be installed on the target machine:
-###    -stress can be installed using 'sudo apt-get install stress'
-###    -cpulimit can be installed using 'sudo apt-get install cpulimit'
-### 
-###
-### Usage: ./cpuload.sh [cpu load in percent] [duration in seconds]                                      
-###        ./cpuload.sh 25 10
-###
+# # CPU load script 
+
+# This script generates a desired CPU load and forces it per each core on 
+# machines running Ubuntu.
+
+# It requires stress and cpulimit to be installed on the target machine:
+#    sudo apt-get install stress
+#    sudo apt-get install cpulimit
+
+# Usage:
+#    ./cpuload.sh [cpu load in percent] [duration in seconds]
+#    ./cpuload.sh 25 10
 ###############################################################################
 
    # Usage: countdown "00:07:55"
